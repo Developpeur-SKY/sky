8a630ff989fe83c74a44567d16b1b928c0addd43
DEPDEV06\Hatem kam <hatemjami.1920@gmail.com>
2021-03-09 10:52:40 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" href="css/app.css">
+     <script src="js/app.js"></script>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="d-flex" style="margin-right: 2%;justify-content:flex-end;">
+            <button style="background-color: #ffffff; border: 1px solid; margin-right: 1%;" >ajouter votre établissement</button>
+            <button style="background-color: #ffffff; border: 1px solid; margin-right: 1%;" >s'inscrire</button>
+            <button style="background-color: #ffffff; border: 1px solid; margin-right: 1%;">se connecter</button>
+        </div>  
+
+        <div style="margin-left: 2%;">
+        <h1 >SKY</h1>
+        </div>
+
+        <div class="d-flex" style="margin-left: 6%; justify-content:end;margin-top: 2%;" >
+            <button style="background-color: #ffffff; border: 1px solid; width: 4%;   margin-right: 1%;" >Vol</button>
+            <button style="background-color: #ffffff; border: 1px solid; width: 4%;  margin-right: 1%;" > Hotels </button>
+            <button style="background-color: #ffffff; border: 1px solid;  width: 12%;   margin-right: 1%;">Locations saisonnières</button>
+            <button style="background-color: #ffffff; border: 1px solid; width: 8%;   margin-right: 1%;" >Location de Voitures</button>
+            <button style="background-color: #ffffff; border: 1px solid;  width: 8%;   margin-right: 1%;">Sorties</button>
+        </div>  
+
+        <div class="d-flex" style="margin-left: 3%; justify-content:end;margin-top: 2%;" >
+            <button style="background-color: #ffffff; border: 1px solid; width: 8%;   margin-right: 1%;" >Où allez vous ?</button>
+            <button style="background-color: #ffffff; border: 1px solid; width: 5%;  margin-right: 1%;" > Arrivée </button>
+            <button style="background-color: #ffffff; border: 1px solid; width: 8%;   margin-right: 1%;"> Départ </button>
+            <button style="background-color: #ffffff; border: 1px solid;  width: 8%;   margin-right: 1%;" >Voyageurs </button>
+            <button style="background-color: #ffffff; border: 1px solid;  width: 8%;   margin-right: 1%;"> Rechercher </button>     
+        </div>  
+            <div  style="margin-left: 3%; margin-top: 2%; ">
+            <input type="checkbox"> Je voyage pour le travail
+            </div>
+    </body>
+</html>
